8348a079cbb533fd26a93e46ac6b64670baee407
Zevi Liao <jointjm@gmail.com>
2019-08-21 00:42:03 +0800

    add project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/operator/share'
+// import { fromEvent } from 'rxjs/Observable/fromEvnet'
+
+
+var observable = Observable.create(observer => {
+  try {
+    observer.next('hey gus!')
+    observer.next('Hello')
+    setInterval(() => {
+      observer.next('I am good')
+    }, 2000)
+    // observer.complete()
+  } catch (err) {
+    observer.error(err)
+  }
+}).share()
+
+//
+
+var observer = observable.subscribe(
+  x => console.log(x),
+  err => console.log('err', err),
+  () => console.log('ok')
+)
+
+// observer.add(observer2)
+
+setTimeout(() => {
+  // observer.unsubscribe()
+  var observer2 = observable.subscribe(x => console.log('subscribe 2', x))
+}, 1000)
+
+export default class Hello extends Component {
+  render() {
+    return <div>Hello</div>
+  }
 }
-
-export default App;
